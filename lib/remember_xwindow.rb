4eaa20622765ca256281792d0b932a6ee17e5787
Ernie Brodeur <ebrodeur@ujami.net>
2013-02-24 23:03:19 -0800

    POC complete.

--- lib/remember_xwindow.rb
+++ lib/remember_xwindow.rb
@@ -1,5 +1,50 @@
 require "remember_xwindow/version"
+require 'bini'
+require 'bini/optparser'
 
 module RememberXwindow
-  # Your code goes here...
+  extend self
+
+  attr_accessor :search
+  attr_accessor :execute
+
+  def is_visible?
+    return true if `xdotool search --onlyvisible #{RememberXwindow.search}` != ""
+    return false
+  end
+
+  def is_focus?
+    return true if window == `xprop -root _NET_ACTIVE_WINDOW`.split(" ").last.to_i(16)
+    return false
+  end
+
+  def hide
+    `xdotool windowunmap #{window} --sync`
+  end
+
+  def show
+    `xdotool windowmap #{window} --sync`
+  end
+
+  def focus
+    `xdotool windowfocus #{window}`
+  end
+
+  def raisew
+    `xdotool windowraise #{window}`
+  end
+
+  def window_ids
+    if !@window_ids
+      @window_ids = `xdotool search #{RememberXwindow.search}`.split
+    end
+    return @window_ids if @window_ids
+    return nil
+  end
+
+  def window
+    return window_ids[0].to_i if window_ids.any?
+    return nil
+  end
 end
+
--- remember_xwindow.gemspec
+++ remember_xwindow.gemspec
@@ -8,12 +8,13 @@ Gem::Specification.new do |gem|
   gem.version       = RememberXwindow::VERSION
   gem.authors       = ["Ernie Brodeur"]
   gem.email         = ["ebrodeur@ujami.net"]
-  gem.description   = %q{TODO: Write a gem description}
-  gem.summary       = %q{TODO: Write a gem summary}
-  gem.homepage      = ""
+  gem.description   = "A gem to help remembe/return to a running Xwindow application."
+  gem.summary       = "This is designed to emulate the feature in other os's that binds a single key to a running instance of an app."
+  gem.homepage      = "http://www.github.com/erniebrodeur/remember_xwindow"
 
   gem.files         = `git ls-files`.split($/)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.require_paths = ["lib"]
+  gem.add_runtime_dependency 'bini'
 end
